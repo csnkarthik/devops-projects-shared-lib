b7c1e23cb110ddbdfb965528950d87acb21e126b
csnkarthik <karthik.aspx.cs@gmail.com>
2023-12-03 23:59:07 -0800

    staticCOdeAnalysis.groovy added 2

--- vars/staticCodeAnalysis.groovy
+++ vars/staticCodeAnalysis.groovy
@@ -1,6 +1,5 @@
 def call(credentialsId){    
     withSonarQubeEnv(credentialsId: credentialsId) {
-        //sh 'mvn clear package sonar:sonar'
-        sh 'mvn clear package org.sonarsource.scanner.maven:sonar-maven-plugin:RELEASE:sonar'
+        sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:RELEASE:sonar'
     }
 }
