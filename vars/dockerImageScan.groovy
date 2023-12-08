679893de5b095319a3d1c88b91ae98fe82f872af
csnkarthik <karthik.aspx.cs@gmail.com>
2023-12-07 21:46:56 -0800

    docker image scane step added 3

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,5 +1,5 @@
 def call(String project, String hubUser){
     sh """
-        trivy image tag ${hubUser}/${project}  ${hubUser}/${project}:latest > scan.txt     
+        trivy image tag ${hubUser}/${project}  ${hubUser}/${project}:latest > scan.txt cat scan.txt
     """
 }
