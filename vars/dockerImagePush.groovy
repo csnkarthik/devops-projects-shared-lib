46fd51758edf4cba41cb518fdaa8ccf6e3f2c904
csnkarthik <karthik.aspx.cs@gmail.com>
2023-12-07 22:53:37 -0800

    docker image push lib method added  2

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,6 +1,6 @@
 def call(String imageName, String imageTag, String hubUser){
     withCredentials([usernamePassword(credentialsId: 'docker_credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "docker login -u '$USER' -p $'$PASS'"        
+        sh "docker login -u '$USER' -p '$PASS'"        
     }
     sh "docker image push ${hubUser}/${imageName}:${imageTag}"
     sh "docker image push ${hubUser}/${imageName}:latest"
