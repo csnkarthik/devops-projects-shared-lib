0de931380a3e2cd38630093e5d3ee83ee1738e5d
csnkarthik <karthik.aspx.cs@gmail.com>
2023-12-13 21:52:41 -0800

    docker push moved into withCredentials block

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,7 +1,8 @@
 def call(String imageName, String imageTag, String hubUser){
     withCredentials([usernamePassword(credentialsId: 'docker_credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh "docker login -u '$USER' -p '$PASS'"        
+
+        sh "docker image push ${hubUser}/${imageName}:${imageTag}"
+        sh "docker image push ${hubUser}/${imageName}:latest"
     }
-    sh "docker image push ${hubUser}/${imageName}:${imageTag}"
-    sh "docker image push ${hubUser}/${imageName}:latest"
 }
